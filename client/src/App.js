cd71529105bb86080486ed8c65abf7ad94d8b5cd
sanjanareddy226 <sanjanareddy226@gmail.com>
2020-08-16 13:38:58 +0530

    dashboard transitioning pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,31 @@
 import React from "react";
 import "./App.css";
 import Dashboard from "./components/Dashboard";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Integrations from "./components/route/Integrations";
+import Orders from "./components/route/Orders";
+import Reports from "./components/route/Reports";
+import Customer from "./components/route/Customer";
+
 function App() {
   return (
-    <div className="App">
-      <Dashboard />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <div>
+          <Switch>
+            {/* <Route path="/" component={Home} exact/>
+             <Route path="/about" component={About}/> */}
+            <Route path="/" component={Dashboard} exact />
+            <Route path="/Integrations" component={Integrations} />
+            <Route path="/Orders" component={Orders} />
+            <Route path="/Reports" component={Reports} />
+            <Route path="/Customer" component={Customer} />
+
+            {/* <Route component={Error} /> */}
+          </Switch>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/components/route/Orders.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Orders = () => {
+  return (
+    <div>
+      <h1>orders</h1>
+      <p>orders page body content</p>
+    </div>
+  );
+};
+
+export default Orders;
