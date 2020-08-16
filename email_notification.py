bbcff9ed116f79cc9cfa36b2057e6eabcfda7ca8
drDrozio <ishanss2018@gmail.com>
2020-08-16 12:35:58 +0530

    changed email not

--- email_notification.py
+++ email_notification.py
@@ -1,9 +1,23 @@
 import os
+import sys
+import json
+import requests
 from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
 from email.mime.application import MIMEApplication
 from email.mime.multipart import MIMEMultipart
 import smtplib
+
+
+## Fetching Email Data
+# url=''
+# r=requests.get(url)
+# mail_data=r.json()
+mail_data=json.loads(url)
+mail_text=mail_data['ID']
+mail_text=mail_data['Text']
+mail_img=mail_data['Image']
+
 
 def message(subject="Monthly Report", text="", img=None, attachment=None):
     msg = MIMEMultipart()
